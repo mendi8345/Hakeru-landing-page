6c315ae58041979f7a763c2075ea8f0133483668
mendi perlov <mendi8345@gmail.com>
2020-11-02 14:12:52 +0200

    first  commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+var scrollingRow = document.querySelector("#scrolling-row");
 var body = document.querySelector("#body");
 var cancel = document.querySelector("#cancel");
-var alert = document.querySelector("#sixth");
+var alert = document.querySelector("#alert");
 var backdrop = document.querySelector("#toggle");
 var mobileLinks = document.querySelector(".first-section__mobile-links");
 var mobileLinkInsideNav = document.querySelectorAll(".mpm")
@@ -10,6 +11,26 @@ var desktopInsideMenu = document.querySelectorAll(".main-links__li__inside-nav")
 
 var mobileLinkInsideNav = document.querySelectorAll(".mpm")
 var mobileInsideMenu = document.querySelectorAll(".inside-menu")
+
+var scroll = window.requestAnimationFrame ||
+    function(callback) { window.setTimeout(callback, 1000 / 60) };
+var elementsToShow = document.querySelectorAll('.show-on-scroll');
+
+function loop() {
+
+    elementsToShow.forEach(function(element) {
+        if (isElementInViewport(element)) {
+            element.classList.add('is-visible');
+        } else {
+            element.classList.remove('is-visible');
+        }
+    });
+
+    scroll(loop);
+}
+
+
+window.addEventListener("scroll", loop())
 
 
 backdrop.addEventListener("click", (event) => {
@@ -46,12 +67,44 @@ for (let index = 0; index < mobileLinkInsideNav.length; index++) {
 
     })
 }
-body.addEventListener("load", bodyOnLoad(event))
+body.addEventListener("load", bodyOnLoad())
 
-function bodyOnLoad(event) {
+function bodyOnLoad() {
     alert.classList.add("open");
-    console.log("loading", event)
-        // body.styls
+    var left = 50;
+
+    // body.styls
+
 }
 
 cancel.addEventListener("click", () => { alert.classList.remove("open") })
+
+scrollingRow.addEventListener("load", scrollRow())
+
+function scrollRow() {
+    var left = 1;
+    // while () {
+    //     left += 1
+    //     scrollingRow.style.left = left + "px"
+    //     console.log("loading", scrollingRow.style.left);
+    //     if (left < 2000)
+    //         left = 1
+
+    // }
+}
+
+function isElementInViewport(el) {
+    // special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+    var rect = el.getBoundingClientRect();
+    return (
+        (rect.top <= 0 &&
+            rect.bottom >= 0) ||
+        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
+        (rect.top >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    );
+}
